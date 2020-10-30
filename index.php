0f674ef34e90f9b06d4355822c3b3d99cdcc99bf
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-10-30 16:42:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo "IT@KMITL";
+<html>
+<head><title>:: About me ::</title></head>
+<body>
+<h1>:: Profile ::</h1>
+<h4>Name : Kaiwin Naw-in</h4>
+Nickname : Ultra
+Student ID : 63070013
+<h1>:: My art work ::</h1>
+</br>
+<img src=touch_mirror.jpg width="150" height="200">
+<img src=hat_girls.jpg width="150" height="200">
+<h1>My favorite web </h1>
+Hololive 3th: <a href="https://www.youtube.com/channel/UCCzUftO8KOVkV4wQG1vkUvg" target="_blank">Marine Ch. 宝鐘マリン </a>
+
+<h1>เรื่องตลก</h1>
+<b>ผัวเมียกำลังทะเลาะกัน</b></br>
+<b>เมีย</b> : ตบหน้าผัวไป 1 ที</br>
+<b>ผัว</b> : แน่จริงเอ็งตบอีกทีดิ่….</br>
+<b>เมีย</b> : ตบไปอีก 1 ผัวะ แรงๆ</br>
+<b>ผัว</b> : “ดีนะ…ที่ยังเชื่อฟังคำสั่งผัว ไม่งั้นแกเจ็บตัวแน่”</br>
+
+</body>
+</html>
 ?>
