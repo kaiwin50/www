b43d39dff9ac8dc6af437c823b54bd2baeac2bf5
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-10-30 18:06:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 <head><title>:: About me ::</title></head>
 <body>
 <h1>:: Profile ::</h1>
-  <h4>Name : Kaiwin Naw-in</h4>
-    Nickname : Ultra
-    Student ID : 63070013
+<h4>Name : Kaiwin Naw-in</h4>
+Nickname : Ultra
+Student ID : 63070013
 <h1>:: My art work ::</h1>
 </br>
-  <img src=touch_mirror.jpg width="150" height="200">
-  <img src=hat_girls.jpg width="150" height="200">
-<h1>:: My favorite web ::</h1>
-  Hololive 3th: <a href="https://www.youtube.com/channel/UCCzUftO8KOVkV4wQG1vkUvg" target="_blank">Marine Ch. 宝鐘マリン </a>
-  </br>
+<img src=touch_mirror.jpg width="150" height="200">
+<img src=hat_girls.jpg width="150" height="200">
+<h1>My favorite web </h1>
+Hololive 3th: <a href="https://www.youtube.com/channel/UCCzUftO8KOVkV4wQG1vkUvg" target="_blank">Marine Ch. 宝鐘マリン </a>
+
 <h1>เรื่องตลก</h1>
-<b> ปลุกฉันมาทำไม? </b></br>
   <b>หมอ:</b> ตื่นๆ</br>
-  <b>คนไข้:</b> ปลุกผมทำไมครับหมอ?</br>
-  <b>หมอ:</b> ปลุกให้กินยา</br>
-  <b>คนไข้:</b> ยาไรครับหมอ</br>
-  <b>หมอ:</b> ยานอนหลับ</br>
-</br></br>
+<b>คนไข้:</b> ปลุกผมทำไมครับหมอ?</br>
+<b>หมอ:</b> ปลุกให้กินยา</br>
+<b>คนไข้:</b> ยาไรครับหมอ</br>
+<b>หมอ:</b> ยานอนหลับ</br>
+<b>คนไข้:</b> ไอ่สัดดด</br>
+
 </body>
 </html>
