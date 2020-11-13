c41caa8eb77812a9953db0bcad864c40244046f0
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-14 01:59:04 +0700

    Update update.php

--- update.php
+++ update.php
@@ -13,7 +13,7 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id=2, 3, 4"
 
 
 if (mysqli_query($conn, $sql)) {
