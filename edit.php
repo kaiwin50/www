4f4a41fa474d383788ff8759c93fee1b757844fc
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-14 04:55:21 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,3 +1,21 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'kaiwin50.mysql.database.azure.com', 'kaiwin50@kaiwin50', '7144Eba1', 'ITFlab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+
+$UserID = $_GET['GetID'];
+while($Result = mysqli_fetch_array($res))
+{
+           $UserID = $Result['ID'];
+           $Name = $Result['NAME'];
+           $Comment = $Result['Comment'];
+           $Link = $Result['Link'];
+}
+?>
 <html>
 <head>
 <title>ITF Lab</title>
@@ -9,28 +27,11 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'kaiwin50.mysql.database.azure.com', 'kaiwin50@kaiwin50', '7144Eba1', 'ITFlab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
 <div class="container">
   <div class="row">
     <div class="col m-auto">
       <div class="col mt-5">
 
-<?php
-          while($Result = mysqli_fetch_array($res))
-{
-           $UserID = $Result['ID'];
-           $Name = $Result['NAME'];
-           $Comment = $Result['Comment'];
-           $Link = $Result['Link'];
-?>
             <form action = "update.php" method = "post" id="CommentForm" >
                 Name:<br>
                 <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
@@ -40,9 +41,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
                 <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
                 <input type="submit" id="commentBtn">
             </form>
-<?php
-}
-?>
+
       </div>
     </div>
   </div>
