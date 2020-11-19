af6be14b9d0ee34bf757d195960493a99630c8ec
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-19 15:36:02 +0700

    Update update.php

--- update.php
+++ update.php
@@ -7,14 +7,20 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$UserID = $_GET['ID']
-$name = $_POST['NAME'];
-$comment = $_POST['Comment'];
-$link = $_POST['Link'];
+if(isset($_POST['update']))
+{  
+    $UserID = $_GET['ID']
+    $name = $_POST['NAME'];
+    $comment = $_POST['Comment'];
+    $link = $_POST['Link'];
 
 
-$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$UserID'"
-
+    $sql = "UPDATE guestbook SET NAME='".$name."', Comment='".$comment."', Link='".$link."' WHERE ID='".$UserID."'";
+    $result = mysqli_sql($conn,$sql)
+}
+else
+{
+    header("location:index.php)
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
