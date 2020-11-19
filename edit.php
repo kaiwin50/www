6f2ff751deb2c439d0ec3fac0c71ed87c143b9dc
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-19 17:55:26 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -16,15 +16,15 @@ $UserID = $_GET['GetID'];
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body class="bg-dark">
 <div class="container">
   <div class="row">
     <div class="col m-auto">
       <div class="col mt-5">
-          <div class="card-title bg-light text-dark">
+          <div class="card-title text-dark">
             <h3 class="bg-success text-white text-center">Update Form</h>
         </div>
-        <div class="card-body">
+        <div class="card-body bg-light">
            
              <form action="update.php?ID=<?php echo $UserID ?>" method="post">
                 <input type="text" class="form-control mb-2" placeholder=" Name " name="name">
