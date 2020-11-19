f92c31b07328d44da764333deaab3847609b0812
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-19 17:34:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   </div>
 </div>
 <div align="center">
-  <a href="add.php>"><button type="button" class="btn btn-success">Add++</button></a>
+  <a href="add.php"><button type="button" class="btn btn-success">Add++</button></a>
 </div>
 <?php
 mysqli_close($conn);
