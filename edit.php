c8f27db49662d22d53335cdcf0ea6ca4ad0eeac8
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-19 16:45:13 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,11 +1,7 @@
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'kaiwindata.mysql.database.azure.com', 'kaiwin50@kaiwindata', '7144Eba1', 'ITFlab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+
+require_once("connection.php");
+
 
 $UserID = $_GET['GetID'];
 
