2e44a3118b0cb23f68d91d80a93794b10052b6d7
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-19 18:26:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body class="bg-dark">
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'kaiwindata.mysql.database.azure.com', 'kaiwin50@kaiwindata', '7144Eba1', 'ITFlab', 3306);
