89cf427401be861abda8bc116f2b3de43dc132c6
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-27 09:52:23 +0700

    Update and rename product.php to index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body class="bg-dark">
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'tra-data.mysql.database.azure.com', 'kaiwin50@ktra-data', '7144Eba1', 'itftest', 3306);
+mysqli_real_connect($conn, 'tra-data.mysql.database.azure.com', 'kaiwin50@tra-data', '7144Eba1', 'itftest', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
