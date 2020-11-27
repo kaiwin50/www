0810fa3d94a1906ecf7e146dadea2d4fc21fb592
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-27 10:12:23 +0700

    Update connection2.php

--- connection2.php
+++ connection2.php
@@ -6,5 +6,5 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM shop');
 ?>
