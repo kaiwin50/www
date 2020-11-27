dff4bce99c4e48cea6bac7e18cf63c9d775dc983
kaiwin50 <73681213+kaiwin50@users.noreply.github.com>
2020-11-27 10:46:49 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@ require_once("connection.php");
 $ProductID = $_GET['GetID'];
 
 
-$sql = "DELETE FROM shop WHERE ID=$ProductID";
+$sql = "DELETE FROM guestbook WHERE ID=$ProductID";
 
 if ($conn->query($sql) === TRUE) {
   header("location:index.php");
